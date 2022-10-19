788819922366fb327ef02f5370246cab335693ac
Skulaurun Mrusal <Skulaurun@seznam.cz>
2022-10-19 21:00:51 +0200

    Update copyright

--- src/common/overrides.js
+++ src/common/overrides.js
@@ -1,7 +1,7 @@
 /*
  *
  *   A-Periment - Custom minecraft launcher
- *   Copyright (C) 2020 Adam Charvát
+ *   Copyright (C) 2020 - 2022 Adam Charvát
  *
  *   A-Periment is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with a-periment. If not, see <https://www.gnu.org/licenses/>.
+ *   along with A-Periment. If not, see <https://www.gnu.org/licenses/>.
  *
  */
 
